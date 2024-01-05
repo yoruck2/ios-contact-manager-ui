74e2244ed680f1699d6ee133846d436a9e6e1fe3
yoruck2 <yoruck2@gmail.com>
2024-01-05 15:57:55 +0900

    feat: 연락처 변경 기능 구현 및 클래스 타입으로 변경

--- ios-contact-manager-ui/ios-contact-manager-ui/Model/ContactManager.swift
+++ ios-contact-manager-ui/ios-contact-manager-ui/Model/ContactManager.swift
@@ -5,20 +5,45 @@
 //  Created by dopamint on 1/2/24.
 //
 
-struct ContactManager {
-    
-    var contacts: [Contact]
+import UIKit
 
-    mutating func addContact(contact: Contact) {
+final class ContactManager {
+    var contacts: [Contact]
+    
+    var contactsCount: Int {
+        return contacts.count
+    }
+    
+    init(contacts: [Contact]) {
+        self.contacts = contacts
+    }
+    
+    func loadData() {
+        let jsonDecoder: JSONDecoder = JSONDecoder()
+        guard
+            let dataAsset: NSDataAsset = NSDataAsset(name: "contacts")
+        else {
+            return
+        }
+        do {
+            contacts = try jsonDecoder.decode([Contact].self, from: dataAsset.data)
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func addContact(contact: Contact) {
         contacts.append(contact)
     }
     
-    mutating func deleteContact(contact: Contact) {
-        contacts.removeAll(where: { $0.id == contact.id } )
+    func deleteContact(index: Int) {
+        contacts.remove(at: index)
     }
     
-    mutating func editContact(contact: Contact) {
-        
+    func editContact(contact: Contact) {
+        let contactTdices = contacts.indices
+        let willEditContactID = contactTdices.filter { contacts[$0].id == contact.id }
+        willEditContactID.forEach { contacts[$0] = contact }
     }
 }
 
