5db798bae9d266060e1cce874d4d49f30eb2c66e
yoruck2 <yoruck2@gmail.com>
2024-01-05 15:59:06 +0900

    feat: ContactTableViewCell 스토리보드에 연결

---
+++ ios-contact-manager-ui/ios-contact-manager-ui/View/ContactTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  ContactTableViewCell.swift
+//  ios-contact-manager-ui
+//
+//  Created by dopamint on 1/5/24.
+//
+
+import UIKit
+
+class ContactTableViewCell: UITableViewCell {
+    
+    static let identifier: String = "contactCell"
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subtitleLabel: UILabel!
+    
+    func setupCell(contact: Contact) {
+        titleLabel.text = contact.nameAndAge
+        subtitleLabel.text = contact.phoneNumber
+    }
+}
