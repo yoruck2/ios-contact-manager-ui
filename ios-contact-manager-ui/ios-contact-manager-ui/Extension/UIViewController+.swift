e00695294bd5b3e96d93d6dd5efb5340c7e2b060
Changhyun Kang <changhyun.kyle@gmail.com>
2024-01-09 16:03:31 +0900

    makeAlert() 함수 구현

--- ios-contact-manager-ui/ios-contact-manager-ui/Extension/UIViewController+.swift
+++ ios-contact-manager-ui/ios-contact-manager-ui/Extension/UIViewController+.swift
@@ -5,4 +5,14 @@
 //  Created by 강창현 on 1/9/24.
 //
 
-import Foundation
+import UIKit
+
+extension UIViewController {
+    func makeAlert(message: String, actions: [UIAlertAction]) {
+        let alertController = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        for action in actions {
+            alertController.addAction(action)
+        }
+        present(alertController, animated: true)
+    }
+}
