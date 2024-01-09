b6f3c3f88b9de984ce961f5ae44686532b07cd8f
Changhyun Kang <changhyun.kyle@gmail.com>
2024-01-09 20:20:57 +0900

    feat: Error 케이스 추가 및 LocalizedError 채택

--- ios-contact-manager-ui/ios-contact-manager-ui/Model/ContactError.swift
+++ ios-contact-manager-ui/ios-contact-manager-ui/Model/ContactError.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+enum ContactError: LocalizedError {
+    case nameInputError
+    case ageInputError
+    case phoneNumberInputError
+    
+    var errorDescription: String? {
+        switch self {
+        case .nameInputError:
+            return "입력한 이름 정보가 잘못되었습니다"
+        case .ageInputError:
+            return "입력한 나이 정보가 잘못되었습니다"
+        case .phoneNumberInputError:
+            return "입력한 연락처 정보가 잘못되었습니다"
+        }
+    }
+}
