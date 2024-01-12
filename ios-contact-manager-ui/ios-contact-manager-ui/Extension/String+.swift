19c44c8137788fe0ad68a54530c2f5c7fb44b2bc
Changhyun Kang <changhyun.kyle@gmail.com>
2024-01-12 17:17:11 +0900

    feat: 전화번호 포맷팅 String Extension 구현

--- ios-contact-manager-ui/ios-contact-manager-ui/Extension/String+.swift
+++ ios-contact-manager-ui/ios-contact-manager-ui/Extension/String+.swift
@@ -5,4 +5,47 @@
 //  Created by 강창현 on 1/12/24.
 //
 
-import Foundation
+extension String {
+    func formattingPhoneNumber() -> String {
+        let count = self.count
+        let divider: Character = "-"
+        var phoneNumber = self
+        
+        if phoneNumber.hasPrefix("02") {
+            switch count {
+            case 3...5:
+                phoneNumber.insert(divider, at: phoneNumber.index(phoneNumber.startIndex, offsetBy: 2))
+            case 6...9:
+                phoneNumber.insert(divider, at: phoneNumber.index(phoneNumber.startIndex, offsetBy: 2))
+                phoneNumber.insert(divider, at: phoneNumber.index(phoneNumber.endIndex, offsetBy: 5 - count))
+            case 10:
+                phoneNumber.insert(divider, at: phoneNumber.index(phoneNumber.startIndex, offsetBy: 2))
+                phoneNumber.insert(divider, at: phoneNumber.index(phoneNumber.endIndex, offsetBy: -4))
+            default:
+                break
+            }
+        } else if phoneNumber.hasPrefix("15") || phoneNumber.hasPrefix("16") || phoneNumber.hasPrefix("18") {
+            switch count {
+            case 5...9:
+                phoneNumber.insert(divider, at: phoneNumber.index(phoneNumber.startIndex, offsetBy: 4))
+            default:
+                break
+            }
+        } else {
+            switch count {
+            case 4...6:
+                phoneNumber.insert(divider, at: phoneNumber.index(phoneNumber.startIndex, offsetBy: 3))
+            case 7...10:
+                phoneNumber.insert(divider, at: phoneNumber.index(phoneNumber.startIndex, offsetBy: 3))
+                phoneNumber.insert(divider, at: phoneNumber.index(phoneNumber.endIndex, offsetBy: 6 - count))
+            case 11:
+                phoneNumber.insert(divider, at: phoneNumber.index(phoneNumber.startIndex, offsetBy: 3))
+                phoneNumber.insert(divider, at: phoneNumber.index(phoneNumber.endIndex, offsetBy: -4))
+            default:
+                break
+            }
+        }
+        
+        return phoneNumber
+    }
+}
